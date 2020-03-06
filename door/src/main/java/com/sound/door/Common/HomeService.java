bc6f2b7192b90a6e64ff0edb36130ab9b0418bac
harikku <harikku@naver.com>
2020-03-06 16:04:01 +0900

    ㄴㄴ

---
+++ door/src/main/java/com/sound/door/Common/HomeService.java
@@ -0,0 +1,22 @@
+package com.sound.door.Common;
+
+import java.util.List;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sound.door.Mapper.TestMapper;
+
+@Service
+public class HomeService{
+
+	@Autowired
+	private TestMapper testMapper;
+	
+	public List<SYSDept> sysDeptGet() {
+		return testMapper.sysDeptGet();
+	}
+
+
+}
