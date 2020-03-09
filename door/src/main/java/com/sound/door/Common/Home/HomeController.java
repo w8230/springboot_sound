a06e67e64604fd61d0dc149555deaa93dabbc375
USER <USER@DESKTOP-P8JQQNA>
2020-03-10 08:52:26 +0900

    11

--- door/src/main/java/com/sound/door/Common/Home/HomeController.java
+++ door/src/main/java/com/sound/door/Common/Home/HomeController.java
@@ -4,7 +4,7 @@ package com.sound.door.Common.Home;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-//ㅇㅇ
+
 @Controller
 public class HomeController {
 
